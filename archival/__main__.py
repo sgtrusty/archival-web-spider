bc375497251a3633592b1748d0ba47846f34cb1f
netrules <contact@sgonzalez.tech>
2021-01-03 19:11:37 -0400

    Codecov testing

--- archival/__main__.py
+++ archival/__main__.py
@@ -1,37 +1,24 @@
 #!/usr/bin/env python
 import core as archiver
-import getopt, sys
+import argparse, sys
+
+#def exec(args):
 
 def main():
     print("Are we starting to crawl yet?")
     siteurl = len(sys.argv) > 1 and sys.argv[1] or "http://books.toscrape.com"
 
-    # read commandline arguments, first
-    fullCmdArguments = sys.argv
-
-    # - further arguments
-    argumentList = fullCmdArguments[1:]
-
-    unixOptions = "ho:v"
-    gnuOptions = ["help", "output=", "verbose"]
-    try:
-        arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
-    except getopt.error as err:
-        # output error, and return with an error code
-        print (str(err))
-        sys.exit(2)
-
-    for currentArgument, currentValue in arguments:
-        if currentArgument in ("-v", "--verbose"):
-            print ("enabling verbose mode")
-        elif currentArgument in ("-h", "--help"):
-            print ("displaying help")
-        elif currentArgument in ("-o", "--output"):
-            print (("enabling special output mode (%s)") % (currentValue))
-
+    # Initialize parser
+    parser = argparse.ArgumentParser(description = "Example description here")
+    parser.add_argument("-o", "--Output", help = "enabling special output mode")
+    parser.add_argument("-v", "--Verbose", help = "enabling verbose mode")
+    args = parser.parse_args()
+    
+    if args.Output:
+        print (("enabling special output mode (%s)") % (args.Output))
+    
     archiver.crawl(siteurl)
     print("Done crawling " + siteurl)
-
 
 # https://www.analyticsvidhya.com/blog/2015/10/beginner-guide-web-scraping-beautiful-soup-python/
 # siteurl = "https://www.pythoncentral.io/execute-python-script-file-shell/"
