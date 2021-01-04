79633f7fbb5b58e41ef9517a5bcb2589acac6eab
netrules <contact@sgonzalez.tech>
2021-01-03 23:44:58 -0400

    codecov.yml

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='archival-web-spider', version='1.0', packages=find_packages())
+setup(name='archival_web_spider', version='1.0', packages=find_packages())
 
