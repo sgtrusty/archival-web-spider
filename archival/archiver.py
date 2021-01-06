ed9b0823d61b881e4ec446559df6601dc2bb149e
netrules <contact@sgonzalez.tech>
2021-01-05 20:04:39 -0400

    Trying to reduce code complexity

--- archival/archiver.py
+++ archival/archiver.py
@@ -17,64 +17,50 @@ class Archiver(object):
     def add_directory_icon(self, directory_icon):
         self.directory_icon = directory_icon
 
-    def archive_links(self):
-        fetchedUris = []
-        for elem in tqdm(self.soup.find_all("link"), "Extracting links (css and ico)"):
-            elem_url = elem.attrs.get("href") or None
-            if(not elem_url):
-                # if elem does not contain src or href attribute, just skip
-                continue
-                
-            path_dir = self.directory_style
-            if(elem.attrs.get("type") == "text/css" or (elem["rel"] and elem["rel"][0] == "stylesheet")):
-                if("integrity" in elem.attrs):
-                    elem.attrs.pop("integrity")
-                if("crossorigin" in elem.attrs):
-                    elem.attrs.pop("crossorigin")
-            else:
-                path_dir = self.directory_icon
+    def archive_links_process(self, elem):
+        path_dir = self.directory_style
+        if(elem.attrs.get("type") == "text/css" or (elem["rel"] and elem["rel"][0] == "stylesheet")):
+            if("integrity" in elem.attrs):
+                elem.attrs.pop("integrity")
+            if("crossorigin" in elem.attrs):
+                elem.attrs.pop("crossorigin")
+        else:
+            path_dir = self.directory_icon
+            
+        return path_dir
 
-            elem_url = relative_to_static(self.archival_url, elem_url);
-            if(not elem_url or elem in fetchedUris):
+    def archive_links(self):
+        self.fetchedUris = []
+        for elem in tqdm(self.soup.find_all("link"), "Extracting links (css and ico)"):
+            elem_url = parse_asset_url(elem.attrs.get("href"))
+            if(not elem_url):
                 continue
-                
-            fetchedUris.append(elem_url)
+            
+            path_dir = archive_links_process(elem)
             file_dl = download_asset(elem_url, self.directory_base, path_dir, self.test_scenario)
             elem.attrs["href"] = file_dl
         
         return True
 
     def archive_scripts(self):
-        fetchedUris = []
+        self.fetchedUris = []
         for elem in tqdm(self.soup.find_all("script"), "Extracting scripts (js)"):
-            elem_url = elem.attrs.get("src") or None
+            elem_url = parse_asset_url(elem.attrs.get("src"))
             if(not elem_url):
-                # if elem does not contain src or href attribute, just skip
                 continue
-
-            elem_url = relative_to_static(self.archival_url, elem_url);
-            if(not elem_url or elem in fetchedUris):
-                continue
-                
-            fetchedUris.append(elem_url)
+            
             file_dl = download_asset(elem_url, self.directory_base, self.directory_script, self.test_scenario)
             elem.attrs["src"] = file_dl
         
         return True
 
     def archive_images(self):
-        fetchedUris = []
+        self.fetchedUris = []
         for elem in tqdm(self.soup.find_all("img"), "Extracting images (img)"):
-            elem_url = elem.attrs.get("src") or None
+            elem_url = parse_asset_url(elem.attrs.get("src"))
             if(not elem_url):
-                # if elem does not contain src or href attribute, just skip
                 continue
-
-            elem_url = relative_to_static(self.archival_url, elem_url);
-            if(not elem_url or elem in fetchedUris):
-                continue
-                
-            fetchedUris.append(elem_url)
+            
             file_dl = download_asset(elem_url, self.directory_base, self.directory_image, self.test_scenario)
             elem.attrs["src"] = file_dl
         
@@ -83,12 +69,8 @@ class Archiver(object):
     def archive_urls(self):
         # change all a hrefs
         for elem in tqdm(self.soup.find_all("a"), "Fixing links"):
-            elem_url = elem.attrs.get("href") or None
-            if(not elem_url or self.archival_url == elem_url or elem_url + "/index.html" == self.archival_url or elem_url == "/" or elem_url == "index.html"): # FIXME: not properly impl
-                continue
-
-            elem_url_static = relative_to_static(self.archival_url, elem_url);
-            if(not elem_url_static): # FIXME: debug this and impl. proper fix
+            elem_url_static = parse_nav_url(elem.attrs.get("href"))
+            if(not elem_url_static):
                 continue
             
             print("Fixing url from " + elem_url + " to " + elem_url_static)
@@ -96,6 +78,28 @@ class Archiver(object):
             
         return True
         
+    def parse_asset_url(self, elem_url):
+        if(not elem_url):
+            # if elem does not contain src or href attribute, just skip
+            return None
+            
+        elem_url = relative_to_static(self.archival_url, elem_url);
+        if(not elem_url or elem in fetchedUris):
+            return None
+        fetchedUris.append(elem_url)
+        
+        return elem_url
+    
+    def parse_nav_url(self, elem_url):
+        if(not elem_url or self.archival_url == elem_url or elem_url + "/index.html" == self.archival_url or elem_url == "/" or elem_url == "index.html"): # FIXME: not properly impl
+            return None
+
+        elem_url_static = relative_to_static(self.archival_url, elem_url);
+        if(not elem_url_static): # FIXME: debug this and impl. proper fix
+            return None
+        
+        return elem_url_static
+    
     def driver_start(self, driver_path):
         self.driver = setup_browser(driver_path)
         
